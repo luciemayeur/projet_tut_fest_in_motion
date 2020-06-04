7ba3fed257b50c768d58b7317567b0994acb3fb0
Valentin Vanhaecke <valentinvanh@gmail.com>
2020-06-04 14:16:28 +0200

    Commencement du responsive et menu déroulant

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/* Fonctions de basculement entre français et anglais */
+
 function switchEN(){
     var new_page = window.location.href;
 
@@ -12,4 +14,16 @@ function switchFR(){
     new_page = new_page.replace('/en/','/fr/');
     
     window.location.href = new_page;
+}
+
+/* Fonctions pour dérouler le menu (responsive) */
+
+function deroule_menu(){
+    document.getElementById("icon_menu_ouvert").style.display = "none";
+    document.getElementById("menu_deroulant").style.display = "flex";
+}
+
+function cache_menu(){
+    document.getElementById("icon_menu_ouvert").style.display = "block";
+    document.getElementById("menu_deroulant").style.display = "none";
 }
